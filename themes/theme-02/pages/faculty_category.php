88f1abf75fedcdfd093a095a8d2a5f9379541b64
Ajay Kumar Arya <ajaykumararya963983@gmail.com>
2024-08-13 13:41:54 +0530

    update faculity front

--- themes/theme-02/pages/faculty_category.php
+++ themes/theme-02/pages/faculty_category.php
@@ -24,14 +24,14 @@ if ($get->num_rows()) {
                     foreach ($getItems->result() as $item) {
                         ?>
                         <div class="col-lg-2 col-md-2">
-                            <div style="min-height:1px;border:2px solid red;color:red" class="icon_box text-center  icon_box_style2 box_shadow2 radius_all_10 animation animated fadeInUp"
+                            <div style="min-height:1px;border:2px solid var(--primary);color:var(--primary);border-radius:0" class="icon_box text-center  icon_box_style2 box_shadow2 radius_all_10 animation animated fadeInUp"
                                 data-animation="fadeInUp" data-animation-delay="0.02s" style="animation-delay: 0.02s; opacity: 1;">
 
                                 <div class="<?=$item->field1?> mb-3" style="font-size:30px">
 
                                 </div>
 
-                                <div class="intro_desc" style="color:red">
+                                <div class="intro_desc" style="color:var(--primary)">
                                     <?=$item->field2?>
                                 </div>
                             </div>
