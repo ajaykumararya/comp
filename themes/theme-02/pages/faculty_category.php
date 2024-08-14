d90a03cc5f6e17b0585525a67ba31570e2224e51
Ajay Kumar Arya <ajaykumararya963983@gmail.com>
2024-08-14 15:16:00 +0530

    update sewa newsticker blink css 2

--- themes/theme-02/pages/faculty_category.php
+++ themes/theme-02/pages/faculty_category.php
@@ -24,7 +24,7 @@ if ($get->num_rows()) {
                     foreach ($getItems->result() as $item) {
                         ?>
                         <div class="col-lg-2 col-md-2">
-                            <div style="padding:21px;min-height:1px;border:2px solid var(--primary);color:var(--primary);border-radius:0"
+                            <div style="padding:21px;min-height:1px;box-shadow:none;color:var(--primary);border-radius:0"
                                 class="icon_box text-center  icon_box_style2 box_shadow2 radius_all_10 animation animated fadeInUp"
                                 data-animation="fadeInUp" data-animation-delay="0.02s" style="animation-delay: 0.02s; opacity: 1;">
                                 <?php
