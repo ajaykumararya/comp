c72b591c0b7a112daa7b06514fab5126f41bebb0
Ajay Kumar Arya <ajaykumararya963983@gmail.com>
2024-11-14 17:37:03 +0530

    update beauti theme

--- assets/formats/beautyguru/id-card.php
+++ assets/formats/beautyguru/id-card.php
@@ -25,30 +25,30 @@
         p {
             font-weight: bold;
             color: black;
-            font-size: 14px;
+            font-size: 18px;
             width: 400px;
             /* word-spacing: 1px; */
         }
         #photo {
             z-index: 999;
-            top: 17.05%;
-            left: 22px;
+            top: 32.7%;
+            left: 28px;
         }
     </style>
 </head>
 <body class="position-relative">
     <img id="back-image" class="position-relative" src="{document_path}/id-card.jpg">
     <div class="position-absolute" id="photo">
-        <img src="upload/{image}" style="width:115px;height:150px">
+        <img src="upload/{image}" style="width:166px;height:214px">
     </div>
-    <p class="position-absolute" style="top:15.2%;left:20rem;">{roll_no}</p>
-    <p class="position-absolute" style="top:17.3%;left:36.5%">{student_name}</p>
-    <p class="position-absolute" style="top:19.3%;left:36.5%">{father_name}</p>
-    <p class="position-absolute " style="top:21.5%;left:36.5%">{address}</p>
-    <p class="position-absolute " style="top:23.6%;left:36.5%">{dob}</p>
-    <p class="position-absolute " style="top:25.8%;left:36.5%">{course_name}</p>
-    <p class="position-absolute " style="top:27.9%;left:36.5%">{center_name}</p>
-    <div class="position-absolute" style="top:23.7rem;left:1.7%;">
+    <p class="position-absolute" style="top:30%;left:36%;">{roll_no}</p>
+    <p class="position-absolute" style="top:33.7%;left:36%">{student_name}</p>
+    <p class="position-absolute" style="top:38%;left:36%">{father_name}</p>
+    <p class="position-absolute " style="top:42%;left:36%;width:64%">{address}</p>
+    <p class="position-absolute " style="top:46.5%;left:36%">{dob}</p>
+    <p class="position-absolute " style="top:50.5%;left:36%">{course_name}</p>
+    <p class="position-absolute " style="top:55%;left:36%">{center_name}</p>
+    <div class="position-absolute" style="top:62%;left:43.7%;">
         <img src="upload/images/id_card_{student_id}.png" style="width:110px;height:110px;" alt="">
     </div>
 </body>
