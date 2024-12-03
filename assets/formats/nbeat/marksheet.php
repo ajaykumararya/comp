d1c571e8695b35ffe85765b155dd874532b68720
Ajay Kumar Arya <ajaykumararya963983@gmail.com>
2024-12-03 11:15:51 +0530

    update marksheet of nbeat

--- assets/formats/nbeat/marksheet.php
+++ assets/formats/nbeat/marksheet.php
@@ -43,42 +43,46 @@
             left: 77%;
         }
 
-        /* table#first {
-            color: black;
-            font-size: 12px;
-            border-collapse: collapse;
-        }
-        table td,
-        table th{
-            border : 1px solid black;
-            text-align: center;
-        } */
         table th:nth-child(1),
         table,
         tfoot tr {
-            border: 1px solid transparent;
+            border: 1px solid black;
             text-align: center;
             /* font-weight: bold; */
             padding: 0;
             margin: 0;
             line-height: 0 !important;
         }
-
         .fw {
             font-weight: bold;
         }
-
-           
-
+        table tr:nth-child(1) {
+            padding: 0 !important;
+            margin: 0 !important;
+            line-height: 0 !important;
+        }
+        .b-tb {
+            border-top: 1xp solid black;
+            border-bottom: 1px solid black;
+        }
+        .rmrb {
+            border-right: 0px !important
+        }
+        .rmb {
+            border-left: 0px solid transparent !important;
+        }
+        .lb {
+            border-left: 1px solid black;
+        }
+        table head tr th {
+            color: #0651a4 !important;
+        }
         table tr td {
             line-height: 1.6;
         }
-
         table {
             border-collapse: collapse;
-            border:none
         }
-
         .test {
             border: 1px solid red
         }
@@ -90,44 +94,75 @@
     <div class="position-absolute" id="photo">
         <img src="upload/{image}" style="width:73.3px;height:96px">
     </div>
-    <p class="position-absolute" style="top:2.5%;left:20.3%;width:120px">{serial_no}</p>
-    <p class="position-absolute " style="top:2.5%;left:79.5%;width:120px">{enrollment_no}</p>
+    <p class="position-absolute" style="top:16.4%;left:76.4%;width:120px">{roll_no}</p>
+    <p class="position-absolute " style="top:16.4%;left:21.9%;width:120px">{enrollment_no}</p>
+    <p class="position-absolute" style="top:30.4%;left:45%;width:240px">{student_name}</p>
+    <p class="position-absolute" style="top:33.4%;left:34.9%;width:238px">{father_name}</p>
+    <p class="position-absolute" style="top:36.5%;left:15%;width:240px;line-height:1">{course_name}</p>
 
-    <p class="position-absolute" style="top:26.7%;left:50%;width:100px"><?=date('Y',strtotime($issue_date))?></p>
-    <!-- <p class="position-absolute" style="top:17%;right:15%;font-size:12px">{enrollment_no}</p> -->
-    <p class="position-absolute" style="top:33%;left:5%;width:240px">{student_name}</p>
-    <p class="position-absolute" style="top:33%;left:34.9%;width:238px">{father_name}</p>
+    <p class="position-absolute" style="top:39.4%;left:50%;width:100px"><?=date('M Y',strtotime($issue_date))?></p>
+    <p class="position-absolute" style="top:42.5%;left:14%">{center_name}</p>
     <!-- <p class="position-absolute " style="top:31.1%;left:36.5%">{mother_name}</p> -->
-    <p class="position-absolute" style="top:33%;left:64.5%;width:240px;line-height:1">{course_name}</p>
     <!-- <p class="position-absolute " style="top:35.3%;left:36.5%">{center_name}</p> -->
-    <p class="position-absolute text-capitlize" style="top:68.5%;left: 51.3%;width:140px">{marksheet_duration} {duration_type}</p>
+    <!-- <p class="position-absolute text-capitlize" style="top:68.5%;left: 51.3%;width:140px">{marksheet_duration} {duration_type}</p> -->
     <!-- <p class="position-absolute " style="top:39.6%;left:36.5%">{dob}</p> -->
-    <div class="position-absolute" style="bottom:3.75%;left:44.1%;">
+    <!-- <div class="position-absolute" style="bottom:3.75%;left:44.1%;">
         <img src="upload/images/marksheet_{result_id}.png" style="width:90px;height:110px;" alt="">
-    </div>
-    <div class="position-absolute " style="top:43.1%;left:5%;width:90%">
-        <table id="first" style="width:100%">
-           
+    </div> -->
+    <div class="position-absolute " style="top:50%;left:8%;width:84%">
+        <table id="first" border="0" style="width:100%">
+            <thead>
+                <tr>
+                    <th class="primary" rowspan="2" width="50%" style="text-align:left;padding-left:35px">SUBJECTS</th>
+                    <th class="primary" colspan="2" style="font-size:11px;padding:4px">MAXIMUM MARKS</th>
+                    <th class="primary lb" colspan="2" style="font-size:11px;padding:4px">MINIMUM MARKS</th>
+                    <th class="primary lb" colspan="3" style="font-size:11px;padding:4px">OBTAINED MARKS</th>
+                </tr>
+                <tr>
+                    <th class="primary b-tb" style="font-size:11px">THEORY</th>
+                    <th class="primary b-tb" style="font-size:11px">PRACTICAL</th>
+                    <th class="primary b-tb lb" style="font-size:11px">THEORY</th>
+                    <th class="primary b-tb" style="font-size:11px">PRACTICAL</th>
+                    <th class="primary b-tb lb" style="font-size:11px">TH.</th>
+                    <th class="primary b-tb" style="font-size:11px">PR.</th>
+                    <th class="primary b-tb" style="font-size:11px">TOTAL</th>
+                </tr>
+            </thead>
             <tbody>
                 {marks}
                 <tr>
-                    <td style="width:80px">{subject_code}</td>
-                    <td class="primary lb" style="text-align:left;padding-left:2px;font-size:12.81px;width:430px">{subject_name}
+                    <td class="primary lb" style="text-align:left;padding-left:2px;font-size:12.81px">{subject_name}
                     </td>
-                    <td class="primary lb" style="font-size:12.81px;width:60px">{theory_max_marks}</td>
-                    <td class="primary lb" style="font-size:12.81px;width:70px">{theory_min_marks}</td>
+                    <td class="primary lb" style="font-size:12.81px">{theory_max_marks}</td>
+                    <td class="primary lb" style="font-size:12.81px">{practical_max_marks}</td>
+                    <td class="primary lb" style="font-size:12.81px">{theory_min_marks}</td>
+                    <td class="primary lb" style="font-size:12.81px">{practical_min_marks}</td>
                     <td class="lb fw" style="font-size:12.819px">{theory_total}</td>
+                    <td class="fw" style="font-size:12.81px">{practical_total}</td>
+                    <td class="fw" style="font-size:12.81px">{total}</td>
                 </tr>
                 {/marks}
             </tbody>
+            <tfoot>
+                <tr class="fw">
+                    <td class="primary fw" style="font-size:12.81px">TOTAL</td>
+                    <td class="primary lb fw" style="font-size:12.81px">{total_max_theory}</td>
+                    <td class="primary fw" style="font-size:12.81px">{total_max_practical}</td>
+                    <td class="primary fw lb" style="font-size:12.81px">{total_min_theory}</td>
+                    <td class="primary fw" style="font-size:12.81px">{total_min_practical}</td>
+                    <td class="fw lb" style="font-size:12.81px"></td>
+                    <td></td>
+                    <td class="fw" style="font-size:12.81px">{obtain_total}</td>
+                </tr>
+            </tfoot>
         </table>
     </div>
-    <p class="position-absolute" style="bottom:27%;left:69%;width:67px;font-size:15px">{division}</p>
-    <p class="position-absolute" style="bottom:27%;left:77%;width:68px;font-size:15px">{grade}</p>
-    <p class="position-absolute " style="bottom:20.35%;width:108px;left:5%;font-size:13px">{center_code}</p>
-    <p class="position-absolute " style="bottom:20.3%;left:18.5%;font-size:15px;width:467px;line-height:1">{center_name}</p>
-    <p class="position-absolute " style="bottom:27%;left:86%;font-size:15px;width:70px">{obtain_total}</p>
-    <p class="position-absolute" style="bottom:20.35%;left:77.3%;font-size:15px;width:138px">{issue_date}</p>
+    <!-- <p class="position-absolute" style="bottom:27%;left:69%;width:67px;font-size:15px">{division}</p> -->
+    <p class="position-absolute" style="bottom:15.6%;left:77%;width:68px;font-size:15px">{grade}</p>
+    <!-- <p class="position-absolute " style="bottom:20.35%;width:108px;left:5%;font-size:13px">{center_code}</p> -->
+    <!-- <p class="position-absolute " style="bottom:20.3%;left:18.5%;font-size:15px;width:467px;line-height:1">{center_name}</p> -->
+    <!-- <p class="position-absolute " style="bottom:27%;left:86%;font-size:15px;width:70px">{obtain_total}</p> -->
+    <p class="position-absolute" style="bottom:15.6%;left:20%;font-size:15px;width:138px">{issue_date}</p>
 
 </body>
 
