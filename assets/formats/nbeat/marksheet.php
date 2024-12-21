377aa347bad8f69e3cdc4eed10c0fe791591cb72
Ajay Kumar Arya <ajaykumararya963983@gmail.com>
2024-12-21 14:04:16 +0530

    update marksheet of nbeat

--- assets/formats/nbeat/marksheet.php
+++ assets/formats/nbeat/marksheet.php
@@ -99,7 +99,7 @@
     <p class="position-absolute " style="top:16.4%;left:21.9%;width:120px">{enrollment_no}</p>
     <p class="position-absolute" style="top:30.4%;left:45%;width:240px">{student_name}</p>
     <p class="position-absolute" style="top:33.4%;left:34.9%;width:238px">{father_name}</p>
-    <p class="position-absolute" style="top:36.5%;left:15%;width:240px;line-height:1">{course_name}</p>
+    <p class="position-absolute" style="top:36.5%;left:15%;width:77%;line-height:1">{course_name}</p>
 
     <p class="position-absolute" style="top:39.4%;left:50%;width:100px"><?=date('M Y',strtotime($issue_date))?></p>
     <p class="position-absolute" style="top:42.5%;left:14%">{center_name}</p>
