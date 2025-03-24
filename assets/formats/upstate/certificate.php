7b9cef95732c8b9a0a5b833228137aafc89dffd0
Ajay kumar Arya <ajaykumararya963983@gmail.com>
2025-03-24 17:59:00 +0530

    update

--- assets/formats/upstate/certificate.php
+++ assets/formats/upstate/certificate.php
@@ -38,18 +38,19 @@
 
         #photo {
             z-index: 999;
-            top: 72.35%;
-            left: 7.1rem;
+            top: 75.8%;
+            left: 10.75%;
         }
-        #photo1
-        {
-            top: 47%;
-            left: 80.5%;
+
+        #photo1 {
+            top: 61.7%;
+            left: 78.5%;
         }
-       
-        p{
+
+        p {
             font-weight: bold;
         }
+
         .middle-div {
             width: 60%;
             margin-left: 9rem;
@@ -57,61 +58,40 @@
             /* border:1px solid red; */
             font-weight: bold;
         }
-        .test{
-            border:1px solid red
+
+        .test {
+            border: 1px solid red
         }
-        .text-center{
-            text-align:center
+
+        .text-center {
+            text-align: center
         }
     </style>
 </head>
 
-<body class="position-relative">
-    <img id="back-image" class="position-relative" src="{document_path}/certificate.jpg">
+<body class="position-relative"><?php
+$certificate = ($duration_type == 'month' or ($duration_type == 'year' && $duration == 1)) ? 'certificate' : 'diploma';
+?>
+    <img id="back-image" class="position-relative" src="{document_path}/<?= $certificate ?>.jpg">
     <div class="position-absolute" id="photo">
-        <img src="upload/{image}" style="width: 64px;height: 83px;">
+        <img src="upload/{image}" style="width: 65px;height: 84px;">
     </div>
     <div class="position-absolute" id="photo1">
         <img src="upload/images/student_certificate_{certiticate_id}.png" style="width:96px;height:96px">
     </div>
-    <p class="position-absolute" style="top:34.2%;left:28%;width:210px">{roll_no}</p>
-    <p class="position-absolute " style="top:34%;left:70%;width:300px">{enrollment_no}</p>
+    <p class="position-absolute" style="top:43.2%;left:28%;width:210px">{roll_no}</p>
+    <p class="position-absolute " style="top:43.2%;left:70%;width:300px">{enrollment_no} 23</p>
     <p class="position-absolute" style="top:2.5%;font-size:12px;left:110px">{serial_no}</p>
-    <div class="position-absolute" style="top:28%;left:44%">{student_name}</div>
-    <div class="position-absolute" style="top:32%;left:40%">{father_name}</div>
-    <!-- <div class="position-absolute" style="top:49%;left:26%">{mother_name}</div> -->
-    <!-- <div class="position-absolute" style="top:49%;left:74%">{dob}</div> -->
-    <div class="position-absolute" style="top:44.2%;left:50%;width:350px">{course_name}</div>
-    <!-- <div class="position-absolute" style="top:54.85%;left:28%">{duration} {duration_type}</div> -->
-    <!-- <div class="position-absolute" style="top:54.85%;left:50%">{from_date}</div> -->
-    <!-- <div class="position-absolute" style="top:54.85%;left:70%">{to_date}</div> -->
-    <!-- <div class="position-absolute" style="top:57.85%;left:75%">{exam_conduct_date}</div> -->
+    <div class="position-absolute" style="top:35%;left:44%">{student_name}</div>
+    <div class="position-absolute" style="top:40%;left:40%">{father_name}</div>
+    <div class="position-absolute text-center" style="top:54%;left:37%;width:450px">{course_name}</div>
 
-    <div class="position-absolute text-center" style="top:41.5%;left:12%;width:440px">{center_name}</div>
+    <div class="position-absolute text-center " style="top:51.5%;left:9%;width:440px">{center_name}</div>
+    <div class="position-absolute" style="top:59.5%;left:22%;width:60px">{grade}</div>
 
-    <!-- <div class="position-absolute" style="top:63.83%;left:54%;width:100%">{obtain_total}</div> -->
-    <div class="position-absolute" style="top:48.1%;left:24%;width:60px">{grade}</div>
-
-    <div class="position-absolute" style="top:51.8%;left:20%">Lucknow</div>
-    <div class="position-absolute" style="top:39%;left:22.5%">{cert_session}</div>
-    <div class="position-absolute" style="top:55.3%;left:27%">{createdOn}</div>
-
-    <!-- <div class="position-absolute w-100" style="top:44.4%;padding-left:7px;z-index:9999">
-
-        <div class="middle-div">{student_name}</div>
-
-        <div class="middle-div" style="margin-top:2.7rem">{father_name}</div>
-        <div class="middle-div" style="margin-top:2.7rem">{roll_no}</div>
-        <div class="middle-div" style="margin-top:2.6rem">{course_name}</div>
-        <div class="middle-div" style="margin-top:2.95rem">{session}</div>
-        <div class="middle-div" style="margin-top:.8rem;margin-left:20rem">{duration} {duration_type}</div>
-
-        <div class="middle-div" style="margin-top:3rem;width:75%">{center_name}</div>
-
-
-
-    </div> -->
-    <!-- <p class="position-absolute" style="bottom:7.3%;left:12%">{createdOn}</p> -->
+    <div class="position-absolute" style="top:64.1%;left:18%">Lucknow</div>
+    <div class="position-absolute text-center" style="top:49%;left:18.5%;width:112px">{cert_session}</div>
+    <div class="position-absolute" style="top:68.6%;left:25%">{createdOn}</div>
 </body>
 
 </html>
---
+++ assets/formats/upstate/migration_certificate.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>{name} Admit Card</title>
+    <style>
+        * {
+            font-weight: bold;
+            /* text-transform: uppercase; */
+
+        }
+
+        .text-capitlize {
+            text-transform: uppercase;
+        }
+
+        .position-relative {
+            position: relative;
+        }
+
+        .position-absolute {
+            position: absolute;
+            font-weight: bold;
+        }
+
+        div.position-absolute {
+            font-size: 18px;
+        }
+
+        .w-100 {
+            width: 100%;
+            display: grid;
+        }
+
+        span {
+            font-weight: bold;
+            color: #1a4891;
+            font-size: 14px;
+            display: inline-block;
+        }
+
+        #photo {
+            z-index: 999;
+            top: 86.8%;
+            left: 47%;
+        }
+
+        #photo1 {
+            top: 33.2%;
+            left: 78.4%;
+        }
+
+        p {
+            font-weight: bold;
+        }
+
+        .middle-div {
+            width: 60%;
+            margin-left: 9rem;
+            text-align: center;
+            /* border:1px solid red; */
+            font-weight: bold;
+        }
+
+        .text-center {
+            text-align: center;
+        }
+
+        div {
+            /* text-transform: uppercase !important; */
+        }
+
+        .test {
+            border: 1px solid red
+        }
+
+        #center_signature {
+            z-index: 999;
+            bottom: 11%;
+            left: 65%;
+        }
+
+        .hindi-text {
+            font-size: 25pt !important;
+            font-family: "Noto Sans Devanagari", sans-serif;
+        }
+
+        .t-u {
+            /* text-transform: uppercase */
+        }
+    </style>
+</head>
+
+<body class="position-relative">
+    <img id="back-image" class="position-relative" src="{document_path}/migration-certificate.jpg">
+    <?php
+    $this->ki_theme->generate_qr($id, 'migration', current_url());
+    ?>
+    <div class="position-absolute" id="photo1">
+        <img src="upload/images/migration_{id}.png" style="width:92px;height:86px">
+    </div>
+
+    <p class="position-absolute t-u" style="top:30%;left:42%;width:350px">{name}</p>
+    <p class="position-absolute t-u" style="top:36.3%;left:30%;width:350px">{father_name}</p>
+    <p class="position-absolute " style="top:2.5%;left:13%;width:135px;font-size:12px">{sr_no}</p>
+    <!-- <p class="position-absolute" style="top:4%;left:70%;width:150px;text-align:center">{roll_no}</p> -->
+    <p class="position-absolute " style="top:42.8%;left:20%;width:580px;line-height:1">{institute_name}</p>
+    <p class="position-absolute t-u test" style="top:49%;left:25%;width:505px;line-height:1">{course_name}</p>
+    <p class="position-absolute t-u" style="top:54%;left:60%;width:305px;line-height:1">{course_category}</p>
+
+    <p class="position-absolute " style="top:86%;left:16%;width:135px;">{date}</p>
+   
+
+
+</body>
+
+</html>
