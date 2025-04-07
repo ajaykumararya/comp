a54ab8cb25cf2d2ba0e3a2f331304721f21b5b4e
Ajay kumar Arya <ajaykumararya963983@gmail.com>
2025-04-07 17:56:30 +0530

    update image gallery in theme 06

--- themes/theme-06/pages/intro.php
+++ themes/theme-06/pages/intro.php
@@ -10,33 +10,31 @@
         color: var(--primary-color);
     }
 </style>
-</head>
 
-<body>
 
-    <div class="container mt-4 mb-4">
-        <div class="container-box">
-            <h1 class="title"><?= ES("{$type}_title", '') ?></h1>
-            <p>
-                <?= ES("{$type}_description", '') ?>
-            </p>
+<div class="container mt-4 mb-4">
+    <div class="container-box">
+        <h1 class="title"><?= ES("{$type}_title", '') ?></h1>
+        <p>
+            <?= ES("{$type}_description", '') ?>
+        </p>
 
-            <div class="row">
-                <?php
-                $data = $this->SiteModel->get_contents($type);
-                if ($data->num_rows()):
-                    $index = 1;
-                    foreach ($data->result() as $row):
-                        echo '<div class="col-md-6">
+        <div class="row">
+            <?php
+            $data = $this->SiteModel->get_contents($type);
+            if ($data->num_rows()):
+                $index = 1;
+                foreach ($data->result() as $row):
+                    echo '<div class="col-md-6">
                                 <h3 class="section-title">' . $row->field1 . '</h3>
                                 <p>
                                     ' . $row->field2 . '
                                 </p>
                             </div>';
-                    endforeach;
-                endif;
-                ?>
-            </div>
+                endforeach;
+            endif;
+            ?>
         </div>
-        <br>
     </div>
+    <br>
+</div>
