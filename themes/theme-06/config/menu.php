be7f1c9c7c388f4e6574b225ae0d35e3ef757f3b
Ajay kumar Arya <ajaykumararya963983@gmail.com>
2025-04-07 16:45:57 +0530

    update menu

--- themes/theme-06/config/menu.php
+++ themes/theme-06/config/menu.php
@@ -5,6 +5,12 @@ $menu = array(
     'icon' => array('file', 3),
     'submenu' => array(
         array(
+            'label' => 'List All Course(S) ',
+            'type' => 'list-all-courses',
+            'icon' => array('file', 4),
+            'url' => 'cms/static-page/list-all-courses',
+        ),
+        array(
             'label' => 'About Us',
             'type' => 'about_us',
             'icon' => array('file', 4),
