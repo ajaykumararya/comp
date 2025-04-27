398563f2e409016217b5c545b91f7c4d86f5e3d8
Ajay kumar Arya <ajaykumararya963983@gmail.com>
2025-04-27 19:01:21 +0530

    udpate content

---
+++ themes/edu-01/content.php
@@ -0,0 +1,9 @@
+<section class="padding-lg">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12">
+                    {content}
+                </div>
+            </div>
+        </div>
+    </section>
